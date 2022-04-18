2b02ee4d1133d38e26551c5d95f4a1fec1760c5f
Oludare Odo <darekorex143@gmail.com>
2022-04-18 11:27:15 -0500

    fix: pr changes

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "aws_iam_role" "orchestration_role" {
       "StringEquals":{
         "token.actions.githubusercontent.com:sub": [
           "repo:ipipeline/${var.github_repo_name}:ref:refs/heads/master",
-          "repo:ipipeline/${var.github_repo_name}:pull_request"
+          "repo:ipipeline/${var.github_repo_name}:push"
         ]
       }
     }
