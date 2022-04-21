2fb5fe5bc57b247e439f8571fb5d22076f9ffba2
Oludare Odo <darekorex143@gmail.com>
2022-04-21 15:51:40 -0500

    feat: added new functionality

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ EOF
 
 variable "github_repo_name" {
   description = "The name of the repository to use with assume role policy trust condition"
-  type        = list(string)
+  type        = list(strings)
 }
 
 
