8a5959fe28890a0f99c31512ab16e43c86e47f95
Oludare Odo <darekorex143@gmail.com>
2022-04-26 11:13:11 -0500

    fix: new message

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ EOF
 
 variable "github_repo_name" {
   description = "The name of the repository to use with assume role policy trust condition"
-  type        = list(strings)
+  type        = list
 }
 
 
